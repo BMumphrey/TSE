4e5c304189bb185b5719778c9515a5019964429e
Brodie Mumphrey <mmumph2@gmail.com>
2016-08-24 12:38:17 -0500

    Converted read_tse_data() from period separated format to underscore separated format

--- R/readTSE.R
+++ R/readTSE.R
@@ -32,6 +32,7 @@ read_tse_boxes <- function(filename, n_boxes) {
                         nrows = n_boxes,
                         colClasses = col_classes,
                         na.strings = " ")
+
   ##TSE data files come with an extra column after box information
   ##This trims that null column
   tse_boxes <- subset(tse_boxes, select = Box:Text3)
@@ -39,46 +40,46 @@ read_tse_boxes <- function(filename, n_boxes) {
 
 ##Read TSE data from a formatted TSE file
 ##Returns a data table adhering to tidy data principles
-read.tse.data <- function(filename, n.boxes, lights.on = 7, lights.off = 19) {
+read_tse_data <- function(filename, n_boxes, lights_on = 7, lights_off = 19) {
   ##read in raw TSE data
   ##TSE reports null values as "-"
-  tse.data <- read.csv(filename, skip = 3 + n.boxes, na.strings = "-")
+  tse_data <- read.csv(filename, skip = 3 + n_boxes, na.strings = "-")
 
   #Convert data frame to a tidy data format
-  tse.data <- melt(tse.data,
+  tse_data <- melt(tse_data,
                    id.vars = c(1, 2),
                    variable.name = "Time")
 
   ##Convert time variable into a string to prep for POSIXct conversion
-  tse.data <- mutate(tse.data, Time = as.character(Time))
-  tse.data <- mutate(tse.data, Time = substring(Time, 2, nchar(Time)))
+  tse_data <- mutate(tse_data, Time = as.character(Time))
+  tse_data <- mutate(tse_data, Time = substring(Time, 2, nchar(Time)))
 
   ##Create a new table of unique times to avoid converting timestamps
   ##hundreds of thousands of times
 
-  unique.times <- unique(tse.data$Time)
-  unique.times <- as.data.frame(unique.times)
-  unique.times <- mutate(unique.times,
-                         parsed.times = parse_date_time(unique.times,
+  unique_times <- unique(tse_data$Time)
+  unique_times <- as.data.frame(unique_times)
+  unique_times <- mutate(unique_times,
+                         parsed_times = parse_date_time(unique_times,
                                                         "mdyHM",
                                                         tz = "America/Chicago"
                                                         )
                          )
-  names(unique.times) <- c("Time", "Parsed.Time")
+  names(unique_times) <- c("Time", "Parsed_Time")
 
   ##Add variable for light/dark status
-  unique.times <- mutate(unique.times,
-                         Lights = ifelse(hour(Parsed.Time) %in% lights.on:(lights.off - 1),
+  unique_times <- mutate(unique_times,
+                         Lights = ifelse(hour(Parsed_Time) %in% lights_on:(lights_off - 1),
                                          "Light", "Dark"
                                          )
                          )
 
   ##After conversion, merge back into original data table
-  tse.data <- merge(tse.data, unique.times, by = "Time")
+  tse_data <- merge(tse_data, unique_times, by = "Time")
 
   ##Reorder columns and drop unparsed time
   ##Then sort based on Box, then Parameter, then Time
-  tse.data <- tse.data[, c(2, 3, 5, 4, 6)]
-  names(tse.data) <- c("Box", "Parameter", "Time", "Value", "Lights")
-  tse.data <- arrange(tse.data, Box, Parameter, Time)
+  tse_data <- tse_data[, c(2, 3, 5, 4, 6)]
+  names(tse_data) <- c("Box", "Parameter", "Time", "Value", "Lights")
+  tse_data <- arrange(tse_data, Box, Parameter, Time)
 }
