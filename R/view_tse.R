843f48a0bc91995818ffbbe87cda3d0e8e4a6d0f
Brodie Mumphrey <mmumph2@gmail.com>
2016-08-26 16:58:36 -0500

    Updated plot_tse_line() to allow choice of grouping column

--- R/view_tse.R
+++ R/view_tse.R
@@ -1,11 +1,14 @@
 ##Takes vectorized inputs for each field of a
 ##TSE data frame and plots it using ggplot2
-plot_tse_data <- function(tse_data, parameter,
+plot_tse_line <- function(tse_data, parameter,
                           grouped = TRUE,
+                          group = "Text1",
                           day_range = "all") {
+
   if (day_range[[1]] == "all") {
     day_range = min(tse_data$Day):max(tse_data$Day)
   }
+
 
   #Subset data based on user inputs
   sub_data <- tse_data[tse_data$Parameter == parameter
@@ -15,11 +18,15 @@ plot_tse_data <- function(tse_data, parameter,
   ##unless otherwise specified
   if (grouped == TRUE) {
     sub_data <- aggregate(sub_data$Value,
-                          list(Text1 = sub_data$Text1, Time = sub_data$Time),
+                          list(group = sub_data[[group]], Time = sub_data$Time),
                           FUN = mean)
-    names(sub_data) <- c("Text1", "Time", "Value")
+    names(sub_data) <- c(group, "Time", "Value")
   }
 
-  g <- ggplot(data = sub_data, aes(x = Time, y = Value, color = Text1))
+  group_color <- sub_data[[group]]
+
+  g <- ggplot(data = sub_data, aes(x = Time,
+                                   y = Value,
+                                   color = group_color))
   g + geom_line()
 }
