1111bb4e49ef5e441a2a6929831d5247688634ec
Brodie Mumphrey <mmumph2@gmail.com>
2016-08-27 12:35:58 -0500

    Added a dropdown menu to select the parameter to be plotted

--- R/app.R
+++ R/app.R
@@ -6,16 +6,19 @@ library(reshape2)
 
 ##Test data set until uploading is implemented
 boxes <- read_tse_boxes("../data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 24)
-data <- read_tse_data("../data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 24)
+tse_data <- read_tse_data("../data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 24)
 
 ui <- fluidPage(
   tableOutput("boxes"),
+  selectInput(inputId = "parameter",
+              label = "Parameter",
+              choices = as.character(unique(tse_data$Parameter))),
   plotOutput("plot")
 )
 
 server <- function(input, output) {
   output$boxes <- renderTable(boxes)
-  output$plot <- renderPlot(plot_tse_line(data, "RER"))
+  output$plot <- renderPlot(plot_tse_line(tse_data, input$parameter))
 }
 
 shinyApp(ui = ui, server = server)
