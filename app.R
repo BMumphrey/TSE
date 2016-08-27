5724fef38b024804900abff8bcb78224a78b06c7
Brodie Mumphrey <mmumph2@gmail.com>
2016-08-27 13:33:06 -0500

    Reordering of elements in the app page

--- app.R
+++ app.R
@@ -9,16 +9,16 @@ boxes <- read_tse_boxes("data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 
 tse_data <- read_tse_data("data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 24)
 
 ui <- fluidPage(
-  tableOutput("boxes"),
   selectInput(inputId = "parameter",
               label = "Parameter",
               choices = as.character(unique(tse_data$Parameter))),
-  plotOutput("plot")
+  plotOutput("plot"),
+  tableOutput("boxes")
 )
 
 server <- function(input, output) {
-  output$boxes <- renderTable(boxes)
   output$plot <- renderPlot(plot_tse_line(tse_data, input$parameter))
+  output$boxes <- renderTable(boxes)
 }
 
 shinyApp(ui = ui, server = server)
