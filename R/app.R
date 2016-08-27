067bc58c43a8c1c742ef59fa7b99d85629e4e4fc
Brodie Mumphrey <mmumph2@gmail.com>
2016-08-27 12:28:51 -0500

    Added framework for displaying box information and data plot

--- R/app.R
+++ R/app.R
@@ -1,5 +1,21 @@
-ui <- fluidPage()
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(reshape2)
 
-server <- function(input, output) {}
+##Test data set until uploading is implemented
+boxes <- read_tse_boxes("../data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 24)
+data <- read_tse_data("../data/berthoud_female_cohort1_RYGB_40215 low temp.csv", 24)
+
+ui <- fluidPage(
+  tableOutput("boxes"),
+  plotOutput("plot")
+)
+
+server <- function(input, output) {
+  output$boxes <- renderTable(boxes)
+  output$plot <- renderPlot(plot_tse_line(data, "RER"))
+}
 
 shinyApp(ui = ui, server = server)
